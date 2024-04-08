e3564b335263440dfc051f7c68b5b330a93a39cb
=Tarre <=>
2024-04-08 14:14:08 +0200

    backup sh

---
+++ backup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+databases=$(docker-compose exec mariadb mysql -e "show databases;" | grep -Ev "(Database|information_schema|performance_schema|mysql)")
+
+for db in $databases; do
+  docker-compose exec mariadb mysqldump "$db" | gzip > /backups/"$db"_$(date +%Y%m%d%H%M%S).sql.gz
+done
+
+# Remove 7 days old files
+find /backups/ -type f -mtime +7 -name '*.gz' -execdir rm -- '{}' \;
